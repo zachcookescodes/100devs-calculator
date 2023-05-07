2c749ad4f37a5da7c8ecadecf48e8562ff0d53d5
ZachCooke <zachcookescodes@gmail.com>
2023-05-07 09:07:03 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -20,6 +20,73 @@ const keys = document.querySelector('.calculator-buttons');
         }else{
             // pass to parse method
             // console.log(`thing is ${event}`)
-            console.log(event)
+            // console.log(value)
+            calculator.parseInput(value)
         }
     })
+
+    const calculator = {
+        displayText: '0',
+        prevTotal: null,
+
+        parseInput(value){
+            // have any special buttons been clicked
+            switch(value){
+                case '=' :
+                    this.calcAnswer(this.displayText)
+                    // calculate answer
+                    break;
+                case 'AC':
+                    // clear screen
+                    this.clearAll()
+                    break;
+                case '.':
+                    if(this.displayText == 0){
+                        this.addText('0.')
+                    // pass '0.' into add text method
+                        }else{
+                    // add value to text string
+                        this.addText(value)
+                    }
+                    break;
+                default:
+                // add value to text string
+                this.addText(value)
+                break;
+            }
+            
+        },
+        addText(value){
+            if(this.displayText === '0'){
+                this.displayText = ''
+            }else if(this.prevTotal !== null){
+                this.displayText = this.prevTotal
+                this.prevTotal = null
+            }
+            if(isNaN(+(value)) && isNaN(+(this.displayText))){
+                    /*check whether the last char in display AND the entered value are not numbers*/
+                if(isNaN(this.displayText.slice(-1))){
+                    return;
+                }
+            
+            }
+            this.displayText += value
+            this.outputText(this.displayText)
+            // output display text to screen
+        },
+
+        outputText(text){
+            document.querySelector('.calculator-screen').value = text
+        },
+
+        calcAnswer(equation){
+            let result = Function('return ' + equation)()
+            this.outputText(result)
+        },
+
+        clearAll(){
+            this.displayText = '0',
+            this.prevTotal = null,
+            this.outputText(this.displayText)
+        }
+    }
